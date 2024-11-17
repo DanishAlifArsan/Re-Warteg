78e93c0739a36f9a96205cf83994d1bc161ab505
danish <danishalif16@gmail.com>
2024-11-17 15:20:08 +0700

    add intro cutscene

--- Assets/Script/UI/Menu/Intro.cs
+++ Assets/Script/UI/Menu/Intro.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DialogueEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Intro : MonoBehaviour
 {
-    [SerializeField] NPCConversation conversation;
+    bool endFlag = false;
     public void EndIntro() {
+        if (endFlag)
+        {
+            return;
+        }
         // GameManager.instance.LoadScene(2);
+        endFlag = true; 
         SceneManager.LoadScene(2);
     }
+    // [SerializeField] NPCConversation conversation;   // buat kalau pakai dialog
+
+    // private void Start() {
+    //     ConversationManager.Instance.StartConversation(conversation);
+    // }
+
+    //buat kalau pakai video
+    [SerializeField] private VideoPlayer videoPlayer;
 
     private void Start() {
-        ConversationManager.Instance.StartConversation(conversation);
+        InputManager.instance.playerInput.UI.Action2.performed += SkipCutscene;
+        videoPlayer.loopPointReached += EndCutscene;
+    }
+
+    private void EndCutscene(VideoPlayer source)
+    {
+        EndIntro();
+    }
+
+    private void SkipCutscene(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        EndIntro();
     }
 }
